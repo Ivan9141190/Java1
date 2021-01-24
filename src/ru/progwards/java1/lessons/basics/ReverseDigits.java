fa98d26bd1afcb454cc2499f07862cccb0a1a87a
9141190 <ermolinivan79@mail.ru>
2021-01-24 12:08:09 +0300

    Commit1

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -3,19 +3,19 @@ package ru.progwards.java1.lessons.basics;
 
     public class ReverseDigits {
         public static int reverseDigits(int number) {
-
-            System.out.print(number % 10);
-            System.out.print(number / 10 % 10);
-            System.out.println(number / 100 % 10);
-
-
-            return number;
-
+//получаем сотни
+            int n1 = number / 100;
+//вычитаем сотни, получаем десятки
+            int n2 = (number - n1 * 100) / 10;
+//вычитаем и сотни и десятки, получаем единицы
+            int n3 = number - n1 * 100 - n2 * 10;
+//всё складываем в обратном порядке
+            return n3 * 100  + n2 * 10 + n1;
         }
 
         public static void main(String[] args) {
 
-
+            System.out.println(reverseDigits(123));
 
         }
     }
