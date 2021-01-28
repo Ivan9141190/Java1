ae34bedf3980c6eb728c4ade4e24b3a04bd5ce52
9141190 <ermolinivan79@mail.ru>
2021-01-28 20:19:00 +0300

    треугольники

---
+++ src/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -0,0 +1,19 @@
+package java1.lessons.compare_if_cycles;
+
+public class TriangleInfo {
+    public static boolean isTriangle(int a, int b, int c) {
+        boolean result = a < b + c && b < a + c && c < a + b;
+        return result;
+
+    }
+    public static boolean isRightTriangle(int a, int b, int c){
+        boolean result =  a==b ||  c==b  || a==c   ;
+
+        return result;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(isTriangle(3, 3, 9));
+        System.out.println(isRightTriangle(9,3,8));
+    }
+}
---
+++ src/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -0,0 +1,40 @@
+package java1.lessons.compare_if_cycles;
+
+public class TriangleSimpleInfo {
+    public static int maxSide(int a, int b, int c) {
+
+        if (a >= b && a >= c) {
+            return a;
+        }
+        if (b >= a && b >= c) {
+            return b;
+        }
+        if (c >= a && c >= b) {
+            return c;
+        }
+        return 0;
+    }
+    public static int minSide(int a, int b, int c){
+        if (a <= b && a <= c) {
+            return a;
+        }
+        if (b <= a && b <= c) {
+            return b;
+        }
+        if (c <= a && c <= b) {
+            return c;
+        }
+        return 0;
+    }
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        boolean result = a == b && a == c;
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(maxSide(1,3,3));
+        System.out.println(minSide(1,2,1));
+        System.out.println(isEquilateralTriangle(2,2,2));
+    }
+}
