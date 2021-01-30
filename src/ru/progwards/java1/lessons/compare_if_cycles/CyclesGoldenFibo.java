60a6dff1a613de1b4f2d9c1956be53723d6f2657
9141190 <ermolinivan79@mail.ru>
2021-01-30 22:14:55 +0300

    треугольники

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -17,7 +17,17 @@ public class CyclesGoldenFibo {
 
         return n;
     }
+    public static boolean isGoldenTriangle(int a, int b, int c){
+        boolean result = a==b && (a/c >= 1.61703)  &&  (a/c <= 1.61903)
+        && (b/c >= 1.61703) && (b/c <= 1.61903) || a==c && (a/b >= 1.61703)  &&
+                (a/b <= 1.61903) && (c/b >= 1.61703) && (c/b <= 1.61903) ||
+                a==c && (a/b >= 1.61703)  &&
+                        (a/b <= 1.61903) && (c/b >= 1.61703) && (c/b <= 1.61903) ||
+                b==c && (b/a >= 1.61703)  &&
+                        (b/a <= 1.61903) && (c/a >= 1.61703) && (c/a <= 1.61903);
 
+    return result;
+    }
 
     public static void main(String[] args) {
         fiboNumber(4);
@@ -35,6 +45,6 @@ public class CyclesGoldenFibo {
             n1 = n2;
             System.out.println(n2);
         }
-
+        System.out.println(isGoldenTriangle(3,3,3));
     }
 }
