d8b12e63deeea8e7f6701ea19a32afd066bbee34
9141190 <ermolinivan79@mail.ru>
2021-02-13 17:23:47 +0300

    классы

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -1,43 +1,33 @@
 package ru.progwards.java1.lessons.classes;
-
 public class ComplexNum {
     int a; // действительная часть
     int b; // мнимая часть
-    int c;
-    int d;
-    String num;
 
     public ComplexNum(int a, int b) {
         this.a = a;
         this.b = b;
-
     }
-
-
     @Override
     public String toString() {
-
         return a + "+" + b + "i";
     }
 
-    public ComplexNum add(ComplexNum num1,ComplexNum num2){
-
-        return new ComplexNum(num1.a + num2.a, num1.b + num2.b);
-    }
-public  ComplexNum sub(ComplexNum num1,ComplexNum num2 ){
-
-        return  new ComplexNum(num1.a - num2.a, num1.b - num2.b);
+  public int getA(){
+        return a;
 }
-
-        public static void main (String[]args){
-            ComplexNum aa = new ComplexNum(6, 5);
-            ComplexNum bb = new ComplexNum(3,4);
-
-
-            System.out.println(aa);
-            System.out.println(bb);
-
-            System.out.println(aa.add(aa,bb));
-            System.out.println(aa.sub(bb,aa));
+    public int getB(){
+        return b;
     }
+    public ComplexNum add(ComplexNum num) {
+
+        return new ComplexNum(a + num.getA(), b + num.getB());
     }
+    public static void main (String[]args){
+
+           ComplexNum getB = new ComplexNum(3,4);
+           ComplexNum getA = new ComplexNum(1,5) ;
+
+            System.out.println(getA);
+            System.out.println(getB);
+            System.out.println(getA.add(getB));
+    }}
