e3cedd25ffe7a64dbbd60ae5721a6bace37f2ada
9141190 <ermolinivan79@mail.ru>
2021-02-13 18:57:31 +0300

    классы

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -22,12 +22,30 @@ public class ComplexNum {
 
         return new ComplexNum(a + num.getA(), b + num.getB());
     }
+    public ComplexNum sub(ComplexNum num) {
+        return new ComplexNum(a- num.getA(), b - num.getB());
+    }
+
+    public ComplexNum mul(ComplexNum num)
+    //    умножение комплексных чисел по формуле:
+//            (a + bi) * (c + di) = (a*c - b*d) + (b*c + a*d)i
+    { return new ComplexNum(a* num.getA()-b* num.getB(),b* num.getA()+a* num.getB());
+    }
+    public ComplexNum div(ComplexNum num) {
+       int c = num.getA(); int d = num.getB();
+    // деление комплексных чисел по формуле:
+            return new ComplexNum  ((a*c + b*d)/(c*c+d*d),
+                    (b*c - a*d)/(c*c+d*d));
+    }
     public static void main (String[]args){
 
-           ComplexNum getB = new ComplexNum(3,4);
-           ComplexNum getA = new ComplexNum(1,5) ;
+           ComplexNum complexNumA = new ComplexNum(3,4);
+           ComplexNum complexNumB = new ComplexNum(1,5) ;
 
-            System.out.println(getA);
-            System.out.println(getB);
-            System.out.println(getA.add(getB));
+            System.out.println(complexNumA);
+            System.out.println(complexNumB);
+            System.out.println(complexNumA.add(complexNumB));
+        System.out.println(complexNumA.sub(complexNumB));
+        System.out.println(complexNumA.mul(complexNumB));
+        System.out.println(complexNumA.div(complexNumB));
     }}
