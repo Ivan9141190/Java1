405ccc37c7ba4852abc7d59bad62249ba2ba4166
9141190 <ermolinivan79@mail.ru>
2021-03-17 21:11:10 +0300

    Eratosthenes8

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -37,36 +37,18 @@ public class Eratosthenes {
             }
         }
     }
-//    public boolean isSimple(int n) {
-//        boolean n = true;
-//        for (int i = 2; i <= N; i++) {
-//            n = true;
-//            for (int j = 2; j < i; j++) {
-//                if (i % j == 0) {
-//                    n = false;
-//                    //break;
-//                }
-//            }
-//            if (n) {
-//
-//                System.out.println(i + "  " + n);
-//
-//            } else
-//                System.out.println(i + " " + n);
-//
-//        }
-//
-//        return n ;
-//    }
+    public boolean isSimple(int n) {
+        return n >= 0 && n <= sieve.length ? sieve[n] : false;
+    }
 
     public static void main(String[] args) {
         Eratosthenes era = new Eratosthenes(80);
         era.sift();
         for ( int i = 2; i< era.sieve.length; i++)
 
-          //  System.out.println(i + " " + era.isSimple(i));
+            System.out.println(i + " " + era.isSimple(i));
 
         System.out.println(Arrays.toString(era.sieve));
-      //  System.out.println(era.isSimple(11));
+        System.out.println(era.isSimple(11));
     }}
 
