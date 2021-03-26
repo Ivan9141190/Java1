bb01755ef2bbf6f498ed547dbf8cbdf932ea0b20
9141190 <ermolinivan79@mail.ru>
2021-03-26 19:20:03 +0300

    Eratosthenes16

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -57,33 +57,33 @@ public class Eratosthenes {
             }
         //return n >0 && n <= sieve.length ? sieve[n] : false;
 
-    public static class ArraySort {
-
-        public static void sort(int[] ArraySort){
-            ArraySort = new int[] {121,434,3443};
-
-            for (int i = 0; i < ArraySort.length; i++) {
-                int num = ArraySort[i];
-
-                for (int j = i - 1; j >= 0; j--) {
-                    int leftnumber = ArraySort[j];
-
-                    if (num < leftnumber) {
-                        ArraySort[j + 1] = leftnumber;
-                        ArraySort[j] = num;
-                    } else {
-                        break;
-                    }
-                }
-            }
-
-            for (int n : ArraySort) {
-                System.out.println(n);
-            }
-        }
-
-
-    }
+//      static class ArraySort {
+//
+//        public static void sort(int[] a){
+//        a = new int[] {121,434,3443};
+//
+//            for (int i = 0; i < a.length; i++) {
+//                int num = a[i];
+//
+//                for (int j = i - 1; j >= 0; j--) {
+//                    int leftnumber = a[j];
+//
+//                    if (num < leftnumber) {
+//                        a[j + 1] = leftnumber;
+//                        a[j] = num;
+//                    } else {
+//                        break;
+//                    }
+//                }
+//            }
+//
+//            for (int n : a) {
+//                System.out.println(n);
+//            }
+//        }
+//
+//
+//    }
     public static void main(String[] args) {
         Eratosthenes era = new Eratosthenes(80);
         era.sift();
