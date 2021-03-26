1927d34b77afd4556d0f5e7a74eaec3a9a36ba88
9141190 <ermolinivan79@mail.ru>
2021-03-26 19:56:26 +0300

    Eratosthenes20

---
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -0,0 +1,26 @@
+package ru.progwards.java1.lessons.arrays;
+
+public class ArraySort { public static void sort(int[] a) {
+
+    ArraySort arraySort = new ArraySort();
+
+    a = new int[] {121,54,45,454,};
+    for (int i = 0; i < a.length; i++) {
+        int num = a[i];
+
+        for (int j = i - 1; j >= 0; j--) {
+            int leftnumber = a[j];
+
+            if (num < leftnumber) {
+                a[j + 1] = leftnumber;
+                a[j] = num;
+            } else {
+                break;
+            }
+        }
+    }
+    for (int n : a) {
+        System.out.println(n);
+    }
+}
+}
