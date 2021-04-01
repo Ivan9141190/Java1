5a89d07e32682a993279a536c6a3d4153bd6e39f
9141190 <ermolinivan79@mail.ru>
2021-04-01 20:16:45 +0300

    Eratosthenes38

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -14,7 +14,7 @@ class DIntArray {
         }
 
     public  void add(int num){
-int []arr = {2,3,3};
+int []arr = new int[]{};
         int [] arr3  =new int [arr.length+1];
         System.arraycopy(arr,0,arr3,0,arr.length);
         arr3 [arr3.length-1]= num;
--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -65,7 +65,7 @@ public class Eratosthenes {
         System.out.println(Arrays.toString(a));
         DIntArray dIntArray = new DIntArray();
         System.out.println(Arrays.toString(dIntArray.arr));
-        dIntArray.add(2);
+        dIntArray.add(21);
         System.out.println(Arrays.toString(dIntArray.arr3));
 
         System.out.println();
